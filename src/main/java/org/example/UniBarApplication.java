038312fe7051181feb0f1f96487677fe846c2868
YUG532 <damone3750@gmail.com>
2025-11-22 15:20:29 +0900

    Spring Boot & MongoDB 연결
    도커 컴포즈 파일도 작성했습니다.

---
+++ src/main/java/org/example/UniBarApplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniBarApplication.class, args);
+    }
+}
+
+
---
+++ src/main/java/org/example/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "UniBar Backend is running! Access Frontend at http://localhost:5173";
+    }
+}
+
